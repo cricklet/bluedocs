938b7603f4056c991574260d2c5c4b18c0c8a95d
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-02-28 13:42:24 -0800

    Font, logic, etc

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
-from flask import Flask
-app = Flask(__name__, static_url_path='static')
+import random
+import string
+from flask import Flask, render_template
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', docs_url='/docs')
 
 @app.route('/docs/<doc_id>')
 def page(doc_id):
     return render_template('doc.html', doc_id=doc_id)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
