08803bee565b5463b118721ef0abdfc9ff1672ae
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-02-28 22:49:07 -0800

    Get ready for AWS EB

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python
-RUN pip install -r requirements.txt
-
-CMD gunicorn blue_server:app --log-file -
-
--- application.py
+++ application.py
@@ -1,15 +1,15 @@
 import random
 import string
 from flask import Flask, render_template
-app = Flask(__name__, static_url_path='/static')
+application = Flask(__name__, static_url_path='/static')
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('index.html', docs_url='/docs')
 
-@app.route('/docs/<doc_id>')
+@application.route('/docs/<doc_id>')
 def page(doc_id):
     return render_template('doc.html', doc_id=doc_id)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(host='0.0.0.0', port=8000, debug=True)
