79389d73b3468cb667461ec47c0a322c54460cbc
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-03-04 17:43:23 -0800

    Minify

--- application.py
+++ application.py
@@ -1,7 +1,58 @@
+import os
 import random
 import string
+import shutil
 from flask import Flask, render_template
-application = Flask(__name__, static_url_path='/static')
+from cssmin import cssmin
+from jsmin import jsmin
+from pdb import set_trace
+from pprint import pprint as pp
+
+DEBUG = __name__ == "__main__"
+STATIC_FOLDER = 'static'
+STATIC_MIN_FOLDER = 'static_min'
+
+# Compile a list of files to minify
+subdir_files_per_directory = [
+    [(subdir, filename) for filename in files]
+    for subdir, dirs, files in os.walk(STATIC_FOLDER)
+]
+subdir_files = sum(subdir_files_per_directory, [])
+
+# Minify each!
+for subdir, filename in subdir_files:
+    new_subdir = STATIC_MIN_FOLDER + subdir.lstrip(STATIC_FOLDER)
+
+    path = subdir + '/' + filename
+    new_path = new_subdir + '/' + filename
+
+    # Make sure the folder exists
+    if not os.path.exists(new_subdir):
+        os.makedirs(new_subdir)
+
+    if filename.endswith('.js'):
+        # Minify JS
+        source = open(path).read()
+        source_min = jsmin(source)
+        open(new_path, 'w').write(source_min)
+
+    elif filename.endswith('.css'):
+        # Minify CSS
+        source = open(path).read()
+        source_min = cssmin(source)
+        open(new_path, 'w').write(source_min)
+
+    else:
+        # Or just copy
+        shutil.copyfile(path, new_path)
+
+static_folder = STATIC_FOLDER if DEBUG else STATIC_MIN_FOLDER
+
+application = Flask(
+    __name__,
+    static_url_path='/static',
+    static_folder=static_folder
+)
 
 @application.route('/')
 def home():
@@ -11,5 +62,5 @@ def home():
 def page(doc_id):
     return render_template('doc.html', doc_id=doc_id)
 
-if __name__ == "__main__":
+if DEBUG:
     application.run(host='0.0.0.0', port=8000, debug=True)
